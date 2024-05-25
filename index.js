00ac1965d52b55ccf70a847ce3643a0b931f7c7a
Sahan Maduwantha <125999503+MrMasterOfc@users.noreply.github.com>
2024-05-25 22:53:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const bot = require(__dirname + '/lib/amd');
+
+const start = async () => {
+  try {
+    await bot.init();
+    await bot.DATABASE.sync();
+    await bot.connect();
+    console.log('Bot started successfully!');
+  } catch (error) {
+    console.error('Error starting bot:', error);
+    setTimeout(() => {
+      console.log('Attempting to restart bot...');
+      start();
+    }, 5000);
+  }
+};
+
+start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DEPRECATED BELOW
+/**const bot = require(__dirname + '/lib/amd')
+
+const start = async () => {
+  try {
+    await bot.init()
+    await bot.DATABASE.sync()
+    await bot.connect()
+  } catch (error) {
+    Debug.error(error);
+    start();
+  }
+}
+start();
+*/
