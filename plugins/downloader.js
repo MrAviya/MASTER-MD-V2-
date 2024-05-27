192ef352ef5f4e3f218949dac3d199215b46c33e
Sahan Maduwantha <125999503+MrMasterOfc@users.noreply.github.com>
2024-05-27 07:51:55 -0700

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -1500,7 +1500,7 @@ UserFunction(
 );
 UserFunction(
   {
-    pattern: "song",
+    pattern: "play",
     alias: ["music"],
     desc: "Sends info about the query(of youtube video/audio).",
     category: "downloader",
@@ -1512,7 +1512,7 @@ UserFunction(
       let match = query ? query : message.reply_text;
       var TYPE = match.toLowerCase().includes("doc") ? "document" : "audio";
       if (!match) {
-        return message.reply("*Example*" + prefix + "song back in black*");
+        return message.reply("*Example*" + prefix + "play back in black*");
       }
       let result = ytIdRegex.exec(match) || [];
       let Rmatch = result[0] || false;
@@ -1529,20 +1529,7 @@ UserFunction(
           "*_Can't dowanload, file duration too big_*"
         );
       }
-      await message.send(
-        "\t *𝐌ᴀꜱᴛᴇʀ-𝐌ᴅ • 𝐒ᴏɴɢ 𝐃ᴏᴡɴʟᴏᴀᴅᴇʀ_🎶👨‍💻*   \n\n*🔰𝐓ɪᴛʟᴇ :* " +
-        resukt.title +
-        "\n🔰𝐔ʀʟ : " +
-        resukt.url +
-        "\n*🔰𝐃ᴇꜱᴄʀɪᴘᴛɪᴏɴ :* " +
-        resukt.timestamp +
-        "\n*🔰𝐕ɪᴇᴡꜱ :* " +
-        resukt.views +
-        "\n*🔰𝐔ᴘʟᴏᴀᴅᴇᴅ :* " +
-        resukt.ago +
-        "\n*🔰𝐀ᴜᴛʜᴏʀ :* " +
-        resukt.author.name +
- );
+      await message.send("_Dᴏᴡɴʟᴏᴀᴅɪɴɢ " + data.title + "?_");
       let resulted = await yt.download(result[1], {
         type: "audio",
         quality: "best",
@@ -1796,7 +1783,7 @@ UserFunction(
 );
 UserFunction(
   {
-    pattern: "play",
+    pattern: "song",
     alias: ["audio"],
     desc: "Downloads audio from youtube.",
     category: "downloader",
