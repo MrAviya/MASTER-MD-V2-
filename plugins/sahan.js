94138f6474854f2b9c2a74fd33f53c53e937b138
Sahan Maduwantha <125999503+MrMasterOfc@users.noreply.github.com>
2024-05-27 08:04:31 -0700

    Update sahan.js

--- plugins/sahan.js
+++ plugins/sahan.js
@@ -2,11 +2,11 @@ const { smd, sleep } = require('../lib');
 
 smd({ cmdname: "sahan", type: "developer", info: "info", filename: __filename }, async (citel) => {
   const messages = [
- "> My Name is Sahan Maduwantha🎉✨" ,
-    "> I am 18 Years Old😎✌",
-    "> I am programming student🌹✔. I can coding Html,Javascript And other. this All these creations are my creations.😊💖",
-    "> I am student of Galewela central collage🐥😘",
-    "> Visit My Site https://mr-sahan-ofc.vercel.app/index.html"
+ "> `My Name is Sahan Maduwantha🎉✨`" ,
+    "> `I am 18 Years Old😎✌`",
+    "> `I am programming student🌹✔. I can coding Html,Javascript And other. this All these creations are my creations.😊💖`",
+    "> `I am student of Galewela central collage🐥😘`",
+    "> `Visit My Site` https://mr-sahan-ofc.vercel.app/index.html"
   ];
 
   let editedMessage;
